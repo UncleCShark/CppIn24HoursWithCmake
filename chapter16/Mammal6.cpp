0013d6d0b3236456ce27576cbd6a15ccaa6d17dd
UncleCShark <38818698+UncleCShark@users.noreply.github.com>
2018-12-21 13:02:35 +0100

    Init

---
+++ chapter16/Mammal6.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Mammal {
+public:
+
+    void move() const {
+        std::cout << "Mammal moves one step\n";
+    }
+
+    void move(int distance) const {
+        std::cout << "Mammal moves " << distance << " steps\n";
+    }
+protected:
+    int age;
+    int weight;
+};
+
+class Dog : public Mammal {
+public:
+
+    void move() const {
+        std::cout << "Dog moves 5 steps\n";
+    }
+    //using Mammal::move;
+}; // you may receive a warning that you are hiding a function!
+
+int main() {
+    Mammal bigAnimal;
+    Dog fido;
+    bigAnimal.move();
+    bigAnimal.move(2);
+    fido.move();
+    // If you uncomment next line you should uncomment line 24
+    // fido.move(10); 
+    return 0;
+}
